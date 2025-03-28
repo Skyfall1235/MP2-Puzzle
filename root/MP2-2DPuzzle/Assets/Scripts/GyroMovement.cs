1d8a6c91d5baacba49c138e746548d02f4b8e45c
Wyatt Murray <wyattmurray1235@gmail.com>
2025-03-28 04:05:26 -0400

    making a maze game :)

---
+++ root/MP2-2DPuzzle/Assets/Scripts/GyroMovement.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GyroMovement : MonoBehaviour
+{
+    bool shouldBallMove = true;
+    public UnityEvent resetGyroPosition = new UnityEvent();
+    public float MaxDeviationForReset;
+    public float MaxSpeed;
+    Rigidbody body;
+
+    bool canReset = false;
+    public bool CanReset { get => canReset; set => canReset = value; }
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+    private void Update()
+    {
+        if (shouldBallMove)
+        {
+            ApplyForceFromGyro();
+        }
+    }
+
+    void ApplyForceFromGyro()
+    {
+        //get the qwuaterion from the device
+        Quaternion CurrentDeviceAngle = GyroToUnity();
+
+        //go read below or read the xml summary
+        CustomVector DirectionAndScalar = ConvertGyroToScaled(CurrentDeviceAngle);
+        Vector2 scaledVector = DirectionAndScalar.planeDir * DirectionAndScalar.NormalizedValue;
+        Vector3 FinalizedVector = new Vector3(scaledVector.x, 0f, scaledVector.y);
+
+        //apply that force
+        body.AddForce(FinalizedVector, ForceMode.Force);
+    }
+
+    CustomVector ConvertGyroToScaled(Quaternion rawValue)
+    {
+        float MagnitudeScalar;
+
+        //convert it to euler
+        Vector3 UelerConvert = rawValue.eulerAngles;
+
+        //conver the anle of rotation into a scalrat to deterin the max speed of the ball
+        if(UelerConvert.magnitude > 0)
+        {
+            MagnitudeScalar = (UelerConvert.x + rawValue.eulerAngles.y) / 180;
+            
+        }
+        else
+        {
+            MagnitudeScalar = 0f;
+        }
+        return new CustomVector(UelerConvert,MagnitudeScalar);
+    }
+
+    public struct CustomVector
+    {
+        public Vector2 planeDir;
+        public float NormalizedValue;
+
+        public CustomVector(Vector2 planeDir, float normalizedValue)
+        {
+            this.planeDir = planeDir;
+            NormalizedValue = normalizedValue;
+        }
+    }
+
+    public void PlayerDied()
+    {
+        canReset = true;
+        shouldBallMove = false;
+    }
+    public void ResetPlayer()
+    {
+        resetGyroPosition.Invoke();
+        canReset = false;
+        shouldBallMove = true;
+    }
+
+    private static Quaternion GyroToUnity()
+    {
+        Quaternion q = Input.gyro.attitude;
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+
+}
